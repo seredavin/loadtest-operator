2aed864728c1d3c49a453ea4ba072dd1c0ce50ed
kirillseredavin <kirill@seredavin.ru>
2025-07-26 21:55:47 +0300

    Добавил LoadWorker

--- cmd/main.go
+++ cmd/main.go
@@ -209,6 +209,13 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "LoadManager")
 		os.Exit(1)
 	}
+	if err := (&controller.LoadWorkerReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "LoadWorker")
+		os.Exit(1)
+	}
 	// +kubebuilder:scaffold:builder
 
 	if metricsCertWatcher != nil {
